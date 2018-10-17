6f0b8f42c601a4a7391285c4854a2f4a929398b5
bzuker <brianzuker@gmail.com>
2018-10-17 19:29:43 -0300

    agrego las categorías

--- components/Page.js
+++ components/Page.js
@@ -3,16 +3,19 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
+  page: {
+    overflow: 'hidden'
+  },
   gridContainer: {
     marginTop: '20px'
   }
 });
 
-const Page = props => (
-  <div>
+const Page = ({ classes, children }) => (
+  <div className={classes.page}>
     <Header />
-    <Grid className={props.classes.gridContainer} container spacing={16} justify="center">
-      {props.children}
+    <Grid className={classes.gridContainer} container spacing={16} justify="center">
+      {children}
     </Grid>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 import Grid from '@material-ui/core/Grid';
+import CategoryPicker from '../components/CategoryPicker';
 
 const Index = props => (
   <React.Fragment>
-    <Grid item xs={6}>
-      <p>El flujo va a ser:</p>
-      <p>Elegir cateogría</p>
-      <p>De ahí vamos a tomar una playlist al azar</p>
-      <p>De esa playlist, elegimos temas al azar</p>
-      <p>
-        Lo ponemos y les damos a completar <b>Nombre del artista</b> y <b>Canción</b>
-      </p>
+    <Grid item xs={10}>
+      <CategoryPicker />
     </Grid>
   </React.Fragment>
 );
