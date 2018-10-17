1c9faecd6e89e1100ff8b28f3fdc1d5d1d17bad4
bzuker <brianzuker@gmail.com>
2018-10-17 17:13:44 -0300

    detalle de los pasos a seguir

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import Grid from '@material-ui/core/Grid';
 const Index = props => (
   <React.Fragment>
     <Grid item xs={6}>
-      Este es el texto
+      <p>El flujo va a ser:</p>
+      <p>Elegir cateogría</p>
+      <p>De ahí vamos a tomar una playlist al azar</p>
+      <p>De esa playlist, elegimos temas al azar</p>
+      <p>
+        Lo ponemos y les damos a completar <b>Nombre del artista</b> y <b>Canción</b>
+      </p>
     </Grid>
   </React.Fragment>
 );
